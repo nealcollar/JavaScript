3d300b48dad774fb289755c6e608f85b3d5a408c
Augusto(nealcollar) <mangustotu@hotmail.com>
2020-09-08 11:44:18 -0300

    first commit

---
+++ 1-MeuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu primeiro programa trabalhando com variaveis");
+
+const idade = 26;
+
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadesomada = idade+2;
+console.log(idadesomada);
