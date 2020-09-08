0922c0ed372e2c76ed95f50063e0db04f3b577bf
Augusto(nealcollar) <mangustotu@hotmail.com>
2020-09-08 15:59:05 -0300

    final commit

---
+++ 2-MeuTrabalhoComVariaveis.js
@@ -0,0 +1,10 @@
+//Console.log("Trabalhando com variaveis");
+console.log("Trabalhando com variaveis");
+//Js e case sensitive
+
+const idade = 29;
+const nome = "Augusto"
+
+
+//nao fazer
+//mes = "Janeiro"
